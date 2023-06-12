6cec9883eb433f0b47fb81b68040a43a3e8a2b13
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2023-06-13 00:12:05 +0630

    usgin useCallback

--- src/components/TripList/index.js
+++ src/components/TripList/index.js
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 export default function Index() {
 
     const [trips, setTrips] = useState([])
     const [url , setUrl] = useState("http://localhost:3001/trips")
 
+    let fetchTrips = useCallback(() => {
+        fetch(url)
+        .then( res => res.json() )
+        .then( data => {
+            setTrips(data) 
+        })
+    }, [url])
 
     // useEffect ထဲမှာ  async, await မသုံးသင့်ဘူး။ .then() နဲ့ပဲသုံးသင့်တယ် ။  ေနာက်ပိုင်း မလိုလားအပ်တဲ့ error တွေတက်နိုင်လို့ ။
     useEffect( () => {
-        fetch(url)
-        .then( res => res.json() )
-        .then( data =>{
-            setTrips(data)
-        })
-    }, [url])
+        fetchTrips()
+    }, [fetchTrips])
     
     // dependency ထဲမှာဘာမှမထည့်ထားရင် useEffect() ထဲက code တွေက component ကို စစခြင်း run တဲ့ အချိန်မှာပဲ တစ်ခါ run တယ်
     // eg.[url] , dependency တွေက state တွေ change တိုင်း useEffect() ထဲက code တွေကို ပြန်လုပ်တယ်
